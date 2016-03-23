b9318033e555547b3fa0c891df7ee4b491a31d9b
ketian <dearsummer@zju.edu.cn>
2016-03-23 16:01:37 +0800

    try divide 20

--- maniford-tsne.py
+++ maniford-tsne.py
@@ -8,8 +8,8 @@ indices = ['v23', 'v10', 'v50', 'v114', 'v12', 'v14', 'v21', 'v34', 'v40']
 
 def main(filename):
     data = pd.read_csv(filename, index_col=0)
-    X = data[indices]
-    y = data['target']
+    X = np.array(data[indices])/20.
+    y = np.array(data['target'])
     tsne = TSNE(n_components=2)
     reduced = tsne.fit_transform(X)
     np.savetxt(filename+'.tsne2d.txt', reduced)
