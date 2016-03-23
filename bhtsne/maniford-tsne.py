6c02b63cf59017974eadfcb3610137ec2caa1549
Tao J <tao-j@outlook.com>
2016-03-23 17:30:29 +0800

    backup

--- bhtsne/maniford-tsne.py
+++ bhtsne/maniford-tsne.py
@@ -2,7 +2,6 @@ import sys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.manifold import TSNE
 
 from argparse import ArgumentParser, FileType
 from os.path import abspath, dirname, isfile, join as path_join
